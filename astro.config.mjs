b1b368bc154317735df6f2b2076c0ddf9411b013
Simon <lgtnt@protonmail.com>
2025-03-28 11:01:30 +0100

    add last updated timestamp

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'LightNet',
+			lastUpdated: true,
 			social: {
 				github: 'https://github.com/LightNetDev/lightnet',
 			},
