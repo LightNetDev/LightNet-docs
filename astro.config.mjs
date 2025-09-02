512d4b24801e44c95d5335d051bcef8540c94104
Simon <lgtnt@protonmail.com>
2025-09-02 12:07:40 +0200

    make skills sound less like strict requirements

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ export default defineConfig({
           label: "Start here",
           items: [
             { slug: "start-here/getting-started" },
-            { slug: "start-here/essential-skills" },
+            { slug: "start-here/helpful-skills" },
           ],
         },
         {
